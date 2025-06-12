4b4db1d119b4c66fe24d55619c9b4ab5af2b7d29
Barnabás Gera <gerabarna@gmail.com>
2025-06-12 23:40:02 +0200

    Switch H2 from in memory db to file system db in production mode

--- src/main/java/hu/gerab/greeting/config/DatabaseConfig.java
+++ src/main/java/hu/gerab/greeting/config/DatabaseConfig.java
@@ -4,24 +4,25 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@Profile("!test")  // This bean is active for all profiles except 'test', we use in-memory H2 there
 public class DatabaseConfig {
 
     @Bean
     @Primary
     public DataSource integrationTestDataSource() {
-        // Your production data source configuration
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .setName("greeting")
-                .addScript("classpath:schema.sql")
-                .addScript("classpath:data.sql")
-                .build();
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("org.h2.Driver");
+            dataSource.setUrl("jdbc:h2:file:./.h2/db");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("");
+            return dataSource;
     }
+
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 MERGE INTO persons(id, name, gender, birth_date)
-VALUES (-1,'Will Robinson', 'male', '2030-01-01');
+VALUES (-1,'Will Robinson', 'MALE', '2030-01-01');
+
+MERGE INTO interests(person_id, interest)
+VALUES (-1, 'robots'),(-1, 'geology'),(-1, 'electronics'),(-1, 'space');
